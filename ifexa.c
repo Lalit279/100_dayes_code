2fdbd8daf1752bc8d778e75e05e52da32533e9e8
Lalit Kumar <68238628+Lalit279@users.noreply.github.com>
2020-09-10 23:31:37 +0530

    Add files via upload

---
+++ ifexa.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("/n Enter a number less then 10");
+    scanf("%d",&num);
+    if (num<10)
+    {
+        printf("/n what an obediant servent you are");
+    }
+    
+}
