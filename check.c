8c69845478f8cc57d2fd0d461e1de19f90b99ac6
Lalit Kumar <68238628+Lalit279@users.noreply.github.com>
2020-09-10 23:27:13 +0530

    Add files via upload

---
+++ check.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+
+{
+	int num1,num2;
+	printf("Enter the two integer:\n");
+	scanf("%d%d",&num1,&num2);
+	if(num1==num2)
+	{
+		printf("Result:%d==%d",num1,num2);
+	}
+	else if(num1>num2)
+	{
+		printf("Result:%d>%d",num1,num2);
+	}
+	else
+	{
+		printf("Result:%d<%d",num1,num2);
+	}
+	return 0;
+	}
+
+
